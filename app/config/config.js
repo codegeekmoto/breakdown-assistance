85b49a773ea2aca3af2484c0639095eef78744be
junfreecss <junrey.algario@gmail.com>
2021-05-19 22:56:45 +0800

    update db creds

--- app/config/config.js
+++ app/config/config.js
@@ -5,15 +5,15 @@ module.exports = {
     },
 
     db: {
-        host: "localhost",
-        user: "admin",
-        password: "admin",
-        database: "automobile"
+        // host: "localhost",
+        // user: "admin",
+        // password: "admin",
+        // database: "automobile"
 
-        // host: "cleverclick-db.cpdbqtgiwfwt.us-west-2.rds.amazonaws.com",
-        // user: "postgres",
-        // password: "cleverclick2021",
-        // database: "cleverclick-db"
+        host: "ec2-54-87-112-29.compute-1.amazonaws.com",
+        user: "mzvjosatocpiis",
+        password: "92ead8d71a18754cc75580d1802fda42d16f0315b80ccecebef339ead597d81d",
+        database: "ddgafk3q6ktgs3"
     },
     
     secret: 'dxbhmxzsrqeyycpe'
