3907ed63b80e8adea00284dfee62d676a98cdd11
junfreecss <junrey.algario@gmail.com>
2021-05-29 23:32:44 +0800

    update service api

--- app/controllers/api/service.cont.js
+++ app/controllers/api/service.cont.js
@@ -2,7 +2,7 @@ const model = require('../../models/datasource')
 
 exports.get = async (req, resp) => {
 
-    var services = await model.service.getActiveServices(req.params.id)
+    var services = await model.service.selectAll()//getActiveServices(req.params.id)
 
     return resp.status(200).send({
         success: true,
