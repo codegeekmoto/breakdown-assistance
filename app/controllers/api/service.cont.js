08b135089cbe9fe4fe38f31f585804dead8f6d85
junfreecss <junfreecss2020@gmail.com>
2021-06-14 10:23:18 +0800

    update service add form

--- app/controllers/api/service.cont.js
+++ app/controllers/api/service.cont.js
@@ -15,6 +15,8 @@ exports.get = async (req, resp) => {
 exports.save = async (req, resp) => {
     const { service_id, description, address, location } = req.body
 
+    console.log('req.body', req.body);
+
     try {
         var service = await model.companyService.insert('service_id, user_id, description, address, latlng, activated',
             [ service_id, req.session.user.id, description, address, JSON.stringify(location), true ]
